06545bfebcf415509494232e0cd79a90679980ad
progyadas <progyanature723@gmail.com>
2022-05-08 18:10:29 +0530

    Changes until the end

--- app/src/main/java/com/mine/facedetector/LivePreviewActivity.java
+++ app/src/main/java/com/mine/facedetector/LivePreviewActivity.java
@@ -16,33 +16,56 @@
 
 package com.mine.facedetector;
 
+import android.Manifest;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Camera;
 import android.graphics.Canvas;
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.provider.MediaStore;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.util.SparseIntArray;
 import android.view.GestureDetector;
+import android.view.Surface;
+import android.view.SurfaceHolder;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
+import android.widget.VideoView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.common.annotation.KeepName;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -55,6 +78,35 @@ public final class LivePreviewActivity extends AppCompatActivity {
     Handler handler = new Handler();
     OutputStream outputStream;
     Runnable runnable;
+
+    MediaRecorder mediaRecorder;
+
+    private static final int REQUEST_CODE=1000;
+    private static final int REQUEST_PERMISSION=1000;
+    private static final SparseIntArray ORIENTATIONS=new SparseIntArray();
+    private MediaProjectionManager mediaProjectionManager;
+    private MediaProjection mediaProjection;
+    private VirtualDisplay virtualDisplay;
+    //private MediaProjectionCallback mediaProjectionCallback;
+
+    private int mScreenDensity;
+    private static final int DISPLAY_HEIGHT=1200;
+    private static final int DISPLAY_WIDTH=720;
+
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0,90);
+        ORIENTATIONS.append(Surface.ROTATION_90,0);
+        ORIENTATIONS.append(Surface.ROTATION_180,270);
+        ORIENTATIONS.append(Surface.ROTATION_270,180);
+    }
+
+    private ToggleButton toggleButton;
+    private VideoView videoView;
+    private String videoUri="";
+    SurfaceHolder holder;
+    boolean recording = false;
+    private boolean isPrepared = false;
+    int videoNumber = 0;
     //GestureDetector gestureDetector;
     int delay=0001;
 
@@ -77,6 +129,13 @@ public final class LivePreviewActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate");
 
         setContentView(R.layout.activity_vision_live_preview);
+
+        DisplayMetrics metrics=new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        mScreenDensity=metrics.densityDpi;
+        mediaRecorder = new MediaRecorder();
+        mediaProjectionManager=(MediaProjectionManager)getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+        videoView=findViewById(R.id.videoView);
 
         preview = findViewById(R.id.preview_view);
         if (preview == null) {
@@ -152,14 +211,118 @@ public final class LivePreviewActivity extends AppCompatActivity {
             }*/
 
                 });
+                imgCameraCapture.setOnLongClickListener(new View.OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View v) {
+                        longclick();
+                       /* Snackbar.make(preview,"Permissions",Snackbar.LENGTH_INDEFINITE)
+                                .setAction("ENABLE", new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        ActivityCompat.requestPermissions(LivePreviewActivity.this,new String[]{
+                                                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                                Manifest.permission.RECORD_AUDIO
+                                        },REQUEST_PERMISSION);
+                                    }
+                                }).show();
+                        initRecorder();
+                        recordScreen();*/
+                        Toast.makeText(LivePreviewActivity.this, "Video saved", Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
             }
             else{
                 imgCameraCapture.setVisibility(View.INVISIBLE);
             }
             handler.postDelayed(this,0001);
         }
+
     };
-    public boolean saveImage(String imgName,Bitmap bmap){
+    /*private void recordScreen(){
+
+        if(mediaProjection==null){
+            startActivityForResult(mediaProjectionManager.createScreenCaptureIntent(),REQUEST_CODE);
+            return;
+        }
+        virtualDisplay=createVirtualDisplay();
+        mediaRecorder.start();
+    }
+
+
+    private VirtualDisplay createVirtualDisplay() {
+        return mediaProjection.createVirtualDisplay("LivePreviewActivity",DISPLAY_WIDTH,DISPLAY_HEIGHT,mScreenDensity,
+                DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                mediaRecorder.getSurface(),null,null);
+    }
+    private void initRecorder() {
+        try{
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+
+            videoUri=Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+                    +new StringBuilder("/").append(new SimpleDateFormat("EDMT_RECORD_dd-MM-yyyy-hh_mm_ss")
+                    .format(new Date())).append(".mp4").toString();
+
+            mediaRecorder.setOutputFile(videoUri);
+            mediaRecorder.setVideoSize(DISPLAY_WIDTH,DISPLAY_HEIGHT);
+            mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mediaRecorder.setVideoEncodingBitRate(512*1000);
+            mediaRecorder.setVideoFrameRate(30);
+
+            int rotation=getWindowManager().getDefaultDisplay().getRotation();
+            int orientation=ORIENTATIONS.get(rotation*90);
+            mediaRecorder.setOrientationHint(orientation);
+            mediaRecorder.prepare();
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }*/
+
+
+
+    public void longclick(){
+
+       /* File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "CameraSample");
+
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
+        mediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+        CamcorderProfile cpHigh = CamcorderProfile
+                .get(CamcorderProfile.QUALITY_HIGH);
+        mediaRecorder.setProfile(cpHigh);
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
+        //CGlobal.VIDEO_RECORD_PATH = CGlobal.VIDEO_HOME_PATH + "VID_" + timeStamp;
+        File mediaFile;
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                "VID_"+ timeStamp + ".mp4");
+
+        mediaRecorder.setOutputFile(mediaFile+".mp4");
+        mediaRecorder.setMaxDuration(50000); // 50 seconds
+        mediaRecorder.setMaxFileSize(5000000); */
+       Intent intent=new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT,10);
+        startActivityForResult(intent,1);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        VideoView videoView = new VideoView(this);
+        videoView.setVideoURI(data.getData());
+        videoView.start();
+        builder.setView(videoView).show();
+
+    }
+
+
+
+
+    public boolean saveImage(String imgName, Bitmap bmap){
         Uri ImageCollection=null;
         ContentResolver resolver= getContentResolver();
         if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.Q){
@@ -296,4 +459,38 @@ public final class LivePreviewActivity extends AppCompatActivity {
             cameraSource.release();
         }
     }
+
+   /* private class MediaProjectionCallback extends MediaProjection.Callback {
+        @Override
+        public void onStop() {
+            mediaRecorder.stop();
+            mediaRecorder.reset();
+
+            mediaProjection=null;
+            stopRecordScreen();
+            super.onStop();
+
+        }
+
+    }
+
+    private void stopRecordScreen() {
+        if(virtualDisplay==null)
+            return;
+        virtualDisplay.release();
+        destroyMediaProjection();
+    }
+
+    private void destroyMediaProjection() {
+        if(mediaProjection!=null){
+            mediaProjection.unregisterCallback(mediaProjectionCallback);
+            mediaProjection.stop();
+            mediaProjection=null;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }*/
 }
